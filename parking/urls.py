94ec1d73d103b0e6147cf3e7d7cd0f3ae0c0969b
valuuusha <162372335+valuuusha@users.noreply.github.com>
2025-10-07 13:20:23 +0300

    minor url api fix

--- parking/urls.py
+++ parking/urls.py
@@ -20,7 +20,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/v1/", include("api.urls")),]
+
+    # version 1
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema-v1"),
+    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema-v1"), name="swagger-ui-v1"),
+    path("api/v1/", include("api.urls")),
+]
 
