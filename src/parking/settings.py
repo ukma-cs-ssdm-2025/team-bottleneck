c5626421f50885b7bbf7b08b67ae8882eeef3986
Anton <146825794+MaFiN1337@users.noreply.github.com>
2025-10-08 22:14:23 +0300

    Update settings.py

--- src/parking/settings.py
+++ src/parking/settings.py
@@ -105,7 +105,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files 
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
