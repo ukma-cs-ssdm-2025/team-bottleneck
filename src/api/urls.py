dd112edc2355b61b71b1b7f10655d8137f60a7e5
Anton Piguliak <antonpigulak@gmail.com>
2025-10-17 21:23:15 +0300

    Create login and users' info update endpoints.

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,11 +1,9 @@
 from rest_framework_nested import routers
 from .views import ParkingLotViewSet, SpotViewSet, BookingViewSet, UserViewSet
 
-# Base router
 router = routers.SimpleRouter()
 router.register(r"lots", ParkingLotViewSet, basename="lot")
 
-# Nested router: /lots/{lot_id}/spots/
 lots_router = routers.NestedSimpleRouter(router, r"lots", lookup="lot")
 lots_router.register(r"spots", SpotViewSet, basename="lot-spots")
 
