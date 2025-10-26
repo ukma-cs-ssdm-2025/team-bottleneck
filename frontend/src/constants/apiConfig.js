e3f828b2c2b5bb03411bd3097243cc9354ea93e9
Anton <146825794+MaFiN1337@users.noreply.github.com>
2025-10-26 17:27:53 +0200

    Change frontend for production matters (#220)
    * Delete created_by field bug in code
    
    * Edit frontend files for production

--- frontend/src/constants/apiConfig.js
+++ frontend/src/constants/apiConfig.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api/v1';
+const isDevelopment = process.env.NODE_ENV === 'development';
+const LOCAL_API_URL = "http://127.0.0.1:8000/api/v1";
+const PRODUCTION_API_URL = "/api/v1";
+export const API_BASE_URL = isDevelopment ? LOCAL_API_URL : PRODUCTION_API_URL;
